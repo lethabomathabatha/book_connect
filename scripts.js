c8057b979f3b80352dd07066fd96427ea480e13b
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-05-02 13:09:12 +0200

    added import paths

--- scripts.js
+++ scripts.js
@@ -1,3 +1,18 @@
+// User stories:
+// 1. As a user, I want to view a list of book previews, by title and author, so that I can discover new books to read.
+// 2. As a user, I want an image associated with all book previews so that I can recognize a book by the cover even if I forgot the name.
+// 3. As a user, I want to have the option of reading a summary of the book so that I can decide whether I want to read it.
+// 4. As a user, I want to have the option of seeing the date that a book was published so that I can determine how easy it is to obtain second-hand.
+// 5. As a user, I want to find books based on specific text phrases so that I don’t need to remember the entire title of a book.
+// 6. As a user, I want to filter books by author so that I can find books to read by authors that I enjoy.
+// 7. As a user, I want to filter books by genre so that I can find books to read in genres that I enjoy.
+// 8. As a user, I want to toggle between dark and light modes so that I can use the app comfortably at night.
+
+import { BOOKS_PER_PAGE } from "./data";
+import { authors } from "./data";
+import { genres } from "./data";
+import { books } from "./data";
+
 matches = books
 page = 1;
 
